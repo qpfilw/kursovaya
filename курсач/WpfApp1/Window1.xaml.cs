72ff6396b4bb84d5d21f6af33fe39e59797551c6
qpfilw <pollyfilyushina@gmail.com>
2023-12-07 17:57:50 +0300

    Filling Combobox's from files

--- курсач/WpfApp1/Window1.xaml.cs
+++ курсач/WpfApp1/Window1.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -18,10 +19,93 @@ namespace WpfApp1
     /// </summary>
     public partial class Window1 : Window
     {
+        string[] policlinicsName;
+        string[] doctorsName;
+
         public Window1()
         {
             InitializeComponent();
+            policlinicsName = File.ReadAllLines("C:\\Users\\user\\OneDrive\\Рабочий стол\\kursovaya\\курсач\\policlinic\\policlinic.txt");
+
+            foreach (string policlinics in policlinicsName)
+            {
+                policlinic.Items.Add(policlinics);
+            }
         }
+
+        private void policlinic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+                    doctor.Items.Clear();
+
+                    switch (policlinic.SelectedIndex)
+                    {
+                        case 0:
+                            doctorsName = File.ReadAllLines(@"C:\Users\user\OneDrive\Рабочий стол\kursovaya\курсач\doctors\doctors1.txt");
+                            break;
+                        case 1:
+                            doctorsName = File.ReadAllLines(@"C:\Users\user\OneDrive\Рабочий стол\kursovaya\курсач\doctors\doctors2.txt");
+                            break;
+                        case 2:
+                            doctorsName = File.ReadAllLines(@"C:\Users\user\OneDrive\Рабочий стол\kursovaya\курсач\doctors\doctors3.txt");
+                            break;
+                        case 3:
+                            doctorsName = File.ReadAllLines(@"C:\Users\user\OneDrive\Рабочий стол\kursovaya\курсач\doctors\doctors4.txt");
+                            break;
+                        case 4:
+                            doctorsName = File.ReadAllLines(@"C:\Users\user\OneDrive\Рабочий стол\kursovaya\курсач\doctors\doctors5.txt");
+                            break;
+                        default:
+                            MessageBox.Show("Неправильный выбор поликлиники");
+                            return;
+                    }
+                    foreach (string doctors in doctorsName)
+                    {
+                        doctor.Items.Add(doctors);
+                    }
+        }
+
+        private void doctor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            service.Items.Clear();
+
+            string selectedDoctorFile = "";
+
+            switch (policlinic.SelectedIndex)
+            {
+                case 0:
+                    selectedDoctorFile = @"C:\Users\user\OneDrive\Рабочий стол\kursovaya\курсач\service.txt";
+                    break;
+                case 1:
+                    selectedDoctorFile = @"C:\Users\user\OneDrive\Рабочий стол\kursovaya\курсач\service.txt";
+                    break;
+                case 2:
+                    selectedDoctorFile = @"C:\Users\user\OneDrive\Рабочий стол\kursovaya\курсач\service.txt";
+                    break;
+                case 3:
+                    selectedDoctorFile = @"C:\Users\user\OneDrive\Рабочий стол\kursovaya\курсач\service.txt";
+                    break;
+                case 4:
+                    selectedDoctorFile = @"C:\Users\user\OneDrive\Рабочий стол\kursovaya\курсач\service.txt";
+                    break;
+                default:
+                    MessageBox.Show("Неправильный выбор поликлиники");
+                    return;
+            }
+
+            if (File.Exists(selectedDoctorFile))
+            {
+                string[] services = File.ReadAllLines(selectedDoctorFile);
+                foreach (string serviceLine in services)
+                {
+                    service.Items.Add(serviceLine);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Файл с услугами врача не найден");
+            }
+        }
+
 
         private void PaymentButton(object sender, RoutedEventArgs e)
         {
@@ -31,3 +115,5 @@ namespace WpfApp1
         }
     }
 }
+
+
--- курсач/WpfApp1/Window2_PaymentInformation.xaml.cs
+++ курсач/WpfApp1/Window2_PaymentInformation.xaml.cs
@@ -39,5 +39,8 @@ namespace WpfApp1
         {
 
         }
+
+
+        
     }
 }
